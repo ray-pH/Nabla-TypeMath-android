248655c9498adee113567a4d838f4b2b42ec46f4
ray-pH <rayhan.azizi9@gmail.com>
2021-07-17 20:34:01 +0700

    Fix Allow Permission Button

--- app/src/main/java/com/ph/typemath/MainActivity.kt
+++ app/src/main/java/com/ph/typemath/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
-import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
