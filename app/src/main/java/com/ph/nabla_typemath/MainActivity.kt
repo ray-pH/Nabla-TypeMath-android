ab08aa3f8160f1179bb45594eab543d5a7cb2a80
ray-pH <rayhan.azizi9@gmail.com>
2021-08-19 22:37:44 +0700

    Add service status (On/Off) indicator in MainActivity.kt

--- app/src/main/java/com/ph/nabla_typemath/MainActivity.kt
+++ app/src/main/java/com/ph/nabla_typemath/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.ph.nabla_typemath
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,6 +35,31 @@ class MainActivity : AppCompatActivity() {
         symbolsButton.setOnClickListener {
             val intent = Intent(this, SymbolsActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun isAccessServiceEnabled(context: Context): Boolean {
+        val serviceName = "MathTypeService"
+        val prefString =
+            Settings.Secure.getString(context.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
+        return prefString.contains("${context.packageName}/${context.packageName}.${serviceName}")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        val serviceEnabled  = isAccessServiceEnabled(applicationContext)
+        val serviceInfoOn   = findViewById<TextView>(R.id.service_info_label_on)
+        val serviceInfoOff  = findViewById<TextView>(R.id.service_info_label_off)
+        val serviceInfoDesc = findViewById<TextView>(R.id.service_allow_permission_desc)
+
+        if(serviceEnabled){
+            serviceInfoOn.visibility   = View.VISIBLE
+            serviceInfoOff.visibility  = View.GONE
+            serviceInfoDesc.visibility = View.GONE
+        }else{
+            serviceInfoOn.visibility   = View.GONE
+            serviceInfoOff.visibility  = View.VISIBLE
+            serviceInfoDesc.visibility = View.VISIBLE
         }
     }
 }
